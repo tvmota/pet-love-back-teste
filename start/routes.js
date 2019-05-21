f9e136be1ea6851c775686185b1bde05131ed965
tvmota <tiago.vieira.mota@gmail.com>
2019-05-21 14:35:20 -0300

    Desabilitaçao dos CORS do serviço

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
 |--------------------------------------------------------------------------
@@ -14,29 +14,6 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use("Route");
-const axios = use("axios");
+const Route = use('Route');
 
-Route.get("/", () => {
-  return `
-  <html>
-    <head>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <section>
-        <div class="logo"></div>
-        <div class="title"></div>
-        <div class="subtitle">s
-          <p>AdonisJs simplicity will make you feel confident about your code</p>
-          <p>
-            Don't know where to start? Read the <a href="https://adonisjs.com/docs">documentation</a>.
-          </p>
-      </div>
-      </section>
-    </body>
-  </html>
-  `;
-});
-
-Route.get("cep/:cep", "CepController.show");
+Route.get('cep/:cep', 'CepController.show');
